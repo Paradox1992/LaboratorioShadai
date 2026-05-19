397089018ea1edf20390a34e23f1ebaeb505dbec
Paradox1992 <riveraorud@gmail.com>
2026-05-19 12:30:31 -0600

    feat: Configure HTTPS scheme for production environment and improve middleware trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,16 +5,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
+                Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST |
+                Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |
+                Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
